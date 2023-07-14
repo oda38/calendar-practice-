ec10388fbdd3c932afff63afdf8be2517e63b4ce
oda38 <129665906+oda38@users.noreply.github.com>
2023-07-14 06:46:36 +0000

    add

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,4 +1,21 @@
 class BlogsController < ApplicationController
+  
   def index
+    @blogs = Blog.all
+    @blog = Blog.new
   end
+  
+  def create
+    Blog.create(blog_parameter)
+    redirect_to blogs_path
+  end
+  
+  
+  
+  def blog_parameter
+    params.require(:blog).permit(:title, :content, :start_time)
+  end
+  
+  
+  
 end
